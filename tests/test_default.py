86152eb38a4bc8c68725c227e0b4853e864dba4e
Varut Atthakornpun <Varut.Atthakornpun@sportsbet.com.au>
2016-09-23 09:16:30 +1000

    add session token

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,6 +1,6 @@
 from testinfra.utils.ansible_runner import AnsibleRunner
-import pytest
-import requests
+# import pytest
+# import requests
 
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
@@ -11,12 +11,14 @@ def test_squid_running_and_enabled(Service, SystemInfo):
     if SystemInfo.distribution == 'centos':
         assert squid.is_running
 
+
 def test_squid_dot_conf(File):
     squid = File("/etc/squid/squid.conf")
     assert squid.user == "root"
     assert squid.group == "root"
     assert squid.mode == 0o644
-    assert squid.contains('acl allowed_https_sites ssl::server_name .amazonaws.com')
+    assert squid.contains('allowed_https_sites ssl::server_name .amazonaws')
+
 
 def test_squid_package(Package, SystemInfo):
     squid = Package('squid')
